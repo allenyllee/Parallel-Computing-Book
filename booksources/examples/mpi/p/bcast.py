069cf342d914f630e5606a4888ba8272a3e32b72
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2016-06-16 10:54:14 -0500

    update

--- booksources/examples/mpi/p/bcast.py
+++ booksources/examples/mpi/p/bcast.py
@@ -32,8 +32,10 @@ dsize = 10
 if procid==root:
     buffer = [ 5.0 ] * dsize
 buffer = comm.bcast(obj=buffer,root=root)
-if not reduce( lambda x,y:x and y, [ buffer[i]==5.0 for i in  range(len(buffer)) ] ):
-    print "Something wrong on proc %d: native buffer <<%s>>" % (procid,str(buffer))
+if not reduce( lambda x,y:x and y,
+               [ buffer[i]==5.0 for i in  range(len(buffer)) ] ):
+    print "Something wrong on proc %d: native buffer <<%s>>" \
+        % (procid,str(buffer))
 
 # then with NumPy
 buffer = np.arange(dsize, dtype=np.float64)
@@ -42,5 +44,6 @@ if procid==root:
         buffer[i] = 5.0
 comm.Bcast( buffer,root=root )
 if not all( buffer==5.0 ):
-    print "Something wrong on proc %d: numpy buffer <<%s>>" % (procid,str(buffer))
+    print "Something wrong on proc %d: numpy buffer <<%s>>" \
+        % (procid,str(buffer))
 #snippet end
